6ba7139c95179930e4fb5d956b39fe126d1e7521
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-01 23:02:42 +0300

    Update Dto properties

--- BoxOfficeHitsRentalCore/Dtos/Product/ProductDto.cs
+++ BoxOfficeHitsRentalCore/Dtos/Product/ProductDto.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BoxOfficeHitsRentalCore.Dtos.Product
 {
     public class ProductDto
     {
-        [Key] public int? Id { get; set; }
         [MaxLength(51)] public string Title { get; set; }
         [MaxLength(51)] public string Genre { get; set; }
         [MaxLength(51)] public string Type { get; set; }
