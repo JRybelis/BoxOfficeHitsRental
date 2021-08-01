3f03ec8f435557b329b9205f2c1217960989d19e
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-01 22:54:55 +0300

    Add Catalogue entity

---
+++ BoxOfficeHitsRentalCore/Entities/Catalogue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfficeHitsRentalCore.Entities.Products
+{
+    public class Catalogue
+    {
+        public List<Product> Products { get; set; }
+    }
+}
