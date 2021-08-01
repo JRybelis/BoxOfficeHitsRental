e64ecfe2811a5cfb34d7acd627530c2bb4de785f
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-01 22:55:08 +0300

    Create Dtos

---
+++ BoxOfficeHitsRentalCore/Dtos/CustomerDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfficeHitsRentalCore.Dtos
+{
+    public class CustomerDto
+    {
+        [Key] public int? Id { get; set; }
+        [MaxLength(51)] public string Forename { get; set; }
+        [MaxLength(51)] public string Surname { get; set; }
+        public decimal? AccountBalance { get; set; }
+    }
+}
---
+++ BoxOfficeHitsRentalCore/Dtos/Product/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfficeHitsRentalCore.Dtos.Product
+{
+    public class ProductDto
+    {
+        [Key] public int? Id { get; set; }
+        [MaxLength(51)] public string Title { get; set; }
+        [MaxLength(51)] public string Genre { get; set; }
+        [MaxLength(51)] public string Type { get; set; }
+        [MaxLength(26)] public string Rating { get; set; }
+        public decimal? Price { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
