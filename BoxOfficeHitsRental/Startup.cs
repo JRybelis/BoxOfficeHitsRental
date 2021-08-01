b0d2ed069be0654ac108b23ba89dc5a5f4774e80
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-01 21:21:21 +0300

    Add NuGet packages. Register database connection string.

--- BoxOfficeHitsRental/Startup.cs
+++ BoxOfficeHitsRental/Startup.cs
@@ -26,6 +26,7 @@ namespace BoxOfficeHitsRental
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var defaultConnectionString = Configuration.GetConnectionString("DefaultConnectionString");
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
