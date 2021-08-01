e8c1d538e79ab05b08864f8babd8f0202b343e18
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-01 23:07:43 +0300

    Create entity - dto mapping.

---
+++ BoxOfficeHitsRental/Mappings/MappingsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BoxOfficeHitsRentalCore.Dtos;
+using BoxOfficeHitsRentalCore.Dtos.Product;
+using BoxOfficeHitsRentalCore.Entities;
+using BoxOfficeHitsRentalCore.Entities.Products;
+
+namespace BoxOfficeHitsRental.Mappings
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<CustomerDto, Customer>().ReverseMap();
+        }
+    }
+}
